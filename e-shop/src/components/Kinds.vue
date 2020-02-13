7c7baf05c602a364e57f4735abd556a3cbefbaf4
C4Jay <choolakejay@gmail.com>
2020-02-13 21:56:46 +0530

    kinds-work

--- e-shop/src/components/Kinds.vue
+++ e-shop/src/components/Kinds.vue
@@ -25,16 +25,39 @@
                 </v-hover>
             </v-flex>
             <v-flex xs12 sm4 class="text-center ">
+                <v-hover v-slot:default="{ hover }">
                 <v-card elevation="19">
                 <v-img height="498" src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-34-15-rBVaJFk4uSOAGEMUAAhFTmoqm88727.jpg/multi-color-8mm-polymer-clay-bracelets-for.jpg">
+                <v-expand-transition>
+                <div
+                 v-if="hover"
+                 class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+                 style="height: 100%;"
+                 >
+                Earrings
+               </div>
+                 </v-expand-transition>
                 </v-img>
                 </v-card>
+                </v-hover>
             </v-flex>
             <v-flex xs12 sm4 class="text-center ">
+                
+                <v-hover v-slot:default="{ hover }">
                 <v-card elevation="19">
                 <v-img height="498" src="https://www.papernstitchblog.com/wp-content/uploads/2018/06/how-to-make-clay-jewelry-earrings-feature.jpg">
+                <v-expand-transition>
+                <div
+                 v-if="hover"
+                 class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+                 style="height: 100%;"
+                 >
+                Earrings
+               </div>
+                 </v-expand-transition>
                 </v-img>
                 </v-card>
+                </v-hover>
             </v-flex>
         </v-layout>
     </v-container>
