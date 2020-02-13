c423f0125b1f1c23b55c43e747939635dfff5ce6
C4Jay <choolakejay@gmail.com>
2020-02-13 20:59:31 +0530

    hover

--- e-shop/src/components/Kinds.vue
+++ e-shop/src/components/Kinds.vue
@@ -8,10 +8,21 @@
         </v-layout>
         <v-layout row wrap>
             <v-flex xs12 sm4 class="text-center ">
+                <v-hover v-slot:default="{ hover }">
                 <v-card elevation="19">
                 <v-img height="498" src="https://i.pinimg.com/736x/44/23/72/442372d66b37e9d909b6dc092e090aeb.jpg">
+                  <v-expand-transition>
+                <div
+                 v-if="hover"
+                 class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+                 style="height: 100%;"
+                 >
+                Earrings
+               </div>
+                 </v-expand-transition>
                 </v-img>
                 </v-card>
+                </v-hover>
             </v-flex>
             <v-flex xs12 sm4 class="text-center ">
                 <v-card elevation="19">
@@ -28,3 +39,23 @@
         </v-layout>
     </v-container>
 </template>
+
+
+
+
+
+
+<style>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .5;
+  position: absolute;
+  width: 100%;
+}
+</style>
+
+
+
+
